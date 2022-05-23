795d9873bf652a12c1381b30ec30daa529db747a
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-05-23 22:19:12 +0200

    Add files via upload

---
+++ easyg.rb
@@ -0,0 +1,35 @@
+#nmap -p 1-65535 -T4 -A -v
+
+require'socket'
+
+puts "\e[35m\n E a s y G\n\e[0m"
+
+c = 0
+
+File.open(ARGV[1],'r').each_line do |f|
+	begin
+		ip=IPSocket::getaddress(f.strip)
+		target = f.gsub("\n","")
+		print target + "\n"
+	rescue
+		ip="unknown"
+	end
+		
+	if ip!="unknown"
+		if ARGV[0] == "firefox"
+			system "start firefox " + target.to_s
+		end
+		
+		if ARGV[0] == "dork"
+			system "start firefox www.google.com/search?q=site%3A" + target.to_s
+		end
+		
+		c += 1
+		
+		if c >= 30
+			sleep 7
+			c = 0
+		end
+		
+	end
+end
