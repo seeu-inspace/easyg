67464c0f0daf9569d492bb1a004505afeb5ceb81
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-07-11 16:11:50 +0200

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -1,40 +1,46 @@
-## Fare opzione sublist3r + firefox
-#nmap -p 1-65535 -T4 -A -v
+# Fare opzione sublist3r + firefox
+# nmap -p 1-65535 -T4 -A -v
 
 require'socket'
 
 puts "\e[35m\n E a s y G\n\e[0m"
 
-c = 0
+if ARGV[0] == "firefox"
+	option = "start firefox "
+end
+		
+if ARGV[0] == "dork"
+	option = "start firefox www.google.com/search?q=site%3A"
+end
+		
+if ARGV[0] == "nmap"
+	option = "nmap -Pn "
+end
 
-File.open(ARGV[1],'r').each_line do |f|
-	begin
-		ip=IPSocket::getaddress(f.strip)
-		target = f.gsub("\n","")
-		print target + "\n"
-	rescue
-		ip="unknown"
-	end
-		
-	if ip!="unknown"
-		if ARGV[0] == "firefox"
-			system "start firefox " + target.to_s
+if option != nil
+
+	c = 0
+
+	File.open(ARGV[1],'r').each_line do |f|
+		begin
+			ip=IPSocket::getaddress(f.strip)
+			target = f.gsub("\n","")
+			print target + "\n"
+		rescue
+			ip="unknown"
 		end
+			
+		if ip!="unknown"
 		
-		if ARGV[0] == "dork"
-			system "start firefox www.google.com/search?q=site%3A" + target.to_s
+			system option + target.to_s
+			
+			c += 1
+			
+			if c >= 30
+				sleep 7
+				c = 0
+			end
+			
 		end
-		
-		if ARGV[0] == "nmap"
-			system "nmap -Pn " + target.to_s
-		end
-		
-		c += 1
-		
-		if c >= 30
-			sleep 7
-			c = 0
-		end
-		
 	end
 end
