a521addb932590d9ce7dd213ad1e3a51c82be7d9
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-07-28 17:23:49 +0200

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -2,23 +2,11 @@ require'socket'
 
 puts "\e[35m\n E a s y G\n\e[0m"
 
-if ARGV[0] == "firefox"
-	option = "start firefox "
-end
-		
-if ARGV[0] == "dork"
-	option = "start firefox www.google.com/search?q=site%3A"
-end
-		
-if ARGV[0] == "nmap"
-	system "nmap -T4 -A -v -iL " + ARGV[1]
-end
-
-if option != nil
+def go_on(option, file_i)
 
 	c = 0
 
-	File.open(ARGV[1],'r').each_line do |f|
+	File.open(file_i,'r').each_line do |f|
 		begin
 			ip=IPSocket::getaddress(f.strip)
 			target = f.gsub("\n","")
@@ -28,7 +16,7 @@ if option != nil
 		end
 			
 		if ip!="unknown"
-		
+			
 			system option + target.to_s
 			
 			c += 1
@@ -42,4 +30,33 @@ if option != nil
 		
 	end
 	
+end
+
+if ARGV[0] == "firefox"
+	option = "start firefox "
+	file_i = ARGV[1]
+	go_on(option, file_i)
+end
+		
+if ARGV[0] == "dork"
+	option = "start firefox www.google.com/search?q=site%3A"
+	file_i = ARGV[1]
+	go_on(option, file_i)
+end
+		
+if ARGV[0] == "nmap"
+	system "nmap -T4 -A -v -iL " + ARGV[1]
+end
+
+if ARGV[0] == "sublist3r"
+	File.open(ARGV[1],'r').each_line do |f|
+		begin
+			target = f.gsub("\n","")
+			print target + "\n"
+		end
+		system "python sublist3r.py -d" + target.to_s + " > " + target.to_s + ".txt"
+		option = "start firefox "
+		file_i = target.to_s + ".txt"
+		go_on(option, file_i)
+	end
 end
