107de12265b3f7bde5ffb66a68144c380abed6c1
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-07-29 15:38:05 +0200

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -7,7 +7,7 @@ def go_on(option, file_i)
 	c = 0
 
 	File.open(file_i,'r').each_line do |f|
-	
+
 		begin
 			ip=IPSocket::getaddress(f.strip)
 			target = f.gsub("\n","")
@@ -15,23 +15,23 @@ def go_on(option, file_i)
 		rescue
 			ip="unknown"
 		end
-			
+
 		if ip!="unknown"
-			
+
 			system option + "http://" + target.to_s
 			system option + "https://" + target.to_s
-			
+
 			c += 1
-			
+
 			if c >= 30
 				sleep 7
 				c = 0
 			end
-			
+
 		end
-		
+
 	end
-	
+
 end
 
 if ARGV[0] == "firefox"
@@ -39,13 +39,7 @@ if ARGV[0] == "firefox"
 	file_i = ARGV[1]
 	go_on(option, file_i)
 end
-		
-if ARGV[0] == "dork"
-	option = "start firefox www.google.com/search?q=site%3A"
-	file_i = ARGV[1]
-	go_on(option, file_i)
-end
-		
+
 if ARGV[0] == "nmap"
 	system "nmap -T4 -A -v -iL " + ARGV[1]
 end
@@ -53,17 +47,17 @@ end
 if ARGV[0] == "sublist3r"
 
 	File.open(ARGV[1],'r').each_line do |f|
-	
+
 		begin
 			target = f.gsub("\n","")
 			print target + "\n"
 		end
-		
+
 		system "python sublist3r.py -d" + target.to_s + " > " + target.to_s + ".txt"
 		option = "start firefox "
 		file_i = target.to_s + ".txt"
 		go_on(option, file_i)
-		
+
 	end
-	
+
 end
