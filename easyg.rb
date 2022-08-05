cfc28bf983246ca14e00105ee43d297bb32d664e
Riccardo Malatesta | riccardomalatesta.eth <92185084+seeu-inspace@users.noreply.github.com>
2022-08-05 16:17:47 +0200

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -2,7 +2,7 @@ require'socket'
 
 puts "\e[35m\n E a s y G\n\e[0m"
 
-def go_on(option, file_i)
+def go_on(file_i)
 
 	c = 0
 
@@ -18,8 +18,8 @@ def go_on(option, file_i)
 			
 		if ip!="unknown"
 			
-			system option + "http://" + target.to_s
-			system option + "https://" + target.to_s
+			system "start firefox " + "http://" + target.to_s
+			system "start firefox " + "https://" + target.to_s
 			
 			c += 1
 			
@@ -59,9 +59,8 @@ if ARGV[0] == "sqlcrawl"
 end
 
 if ARGV[0] == "firefox"
-	option = "start firefox "
 	file_i = ARGV[1]
-	go_on(option, file_i)
+	go_on(file_i)
 end
 
 if ARGV[0] == "sublist3r"
@@ -74,9 +73,8 @@ if ARGV[0] == "sublist3r"
 		end
 		
 		system "python sublist3r.py -d" + target.to_s + " > " + target.to_s + ".txt"
-		option = "start firefox "
 		file_i = target.to_s + ".txt"
-		go_on(option, file_i)
+		go_on(file_i)
 		
 	end
 	
