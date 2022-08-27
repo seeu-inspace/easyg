1286d35f8ddf0fd488914eb9fa878725a4826a65
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-08-27 17:21:58 +0200

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -40,7 +40,7 @@ end
 
 def gau_go_on(file_i)
 
-	system "type " + file_i + " | gau --o " + file_i + "_gau.txt --blacklist svg,png,gif,ico,jpg,bpm,ttf,woff,ttf2,woff2,pptx,pdf"
+	system "type " + file_i + " | httprobe -p http:81 -p http:3000 -p https:3000 -p http:3001 -p https:3001 -p http:8000 -p http:8080 -p https:8443 -c 50 | gau --o " + file_i + "_gau.txt --blacklist svg,png,gif,ico,jpg,bpm,ttf,woff,ttf2,woff2,pptx,pdf"
 
 end
 
@@ -95,11 +95,17 @@ end
 
 if ARGV[0] == "help"
 
-	puts 'Usage: ruby easyg.rb <file_input> <nmap/firefox/firefox-httprobe/gau/amass>'
-	puts 'If amass is selected, you can add <firefox/gau/firefox-gau>' + "\n\n"
+	puts 'Usage: ruby easyg.rb <file_input> <option>'+ "\n\n"
+	puts 'options:'
+	puts ' nmap				perform nmap scan against the domains in the <file_input>'
+	puts ' firefox			open the strings in the <file_input> in firefox'
+	puts ' firefox-httprobe		open the strings in the <file_input> in firefox checking them first with httprobe'
+	puts ' gau				perform gau scan against the strings in the <file_input>'
+	puts ' amass				subdomain discovery | you can add another option <firefox/gau/firefox-gau>'+ "\n\n"
 	
-	puts 'Tested on Windows, if you need to use it on Unix:'
-	puts ' - use `xdg-open` insead of `start firefox`'
-	puts ' - for httprobe, use `cat <file_name> | httprobe`'
+	puts 'Notes:'
+	puts ' - tested on Windows, if you need to use it on Unix:'
+	puts '   > use `xdg-open` insead of `start firefox`'
+	puts '   > for httprobe, use `cat <file_name> | httprobe`'
 	
 end
