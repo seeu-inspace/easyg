6e15778dd1f96c66294187c4fe8e059529537bd4
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-09-24 14:37:06 +0200

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -92,7 +92,7 @@ if ARGV[1] == "paramspider"
 	end
 	
 	puts "[\e[34m+\e[0m] Using trashcompactor on paramspider_results/" + ARGV[0].gsub('.txt','') + "_final.txt"
-	system "type paramspider_results\\final.txt | trashcompactor > paramspider_results/" + ARGV[0].gsub('.txt','') + "_compact.txt"
+	system "type paramspider_results\\"+ ARGV[0].gsub('.txt','') + "_final.txt | trashcompactor > paramspider_results/" + ARGV[0].gsub('.txt','') + "_compact.txt"
 	puts "[\e[34m+\e[0m] saved results in paramspider_results/" + ARGV[0].gsub('.txt','') + "_compact.txt"
 	
 end
