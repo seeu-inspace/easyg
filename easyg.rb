6569f026f08269b741b751a1d974e9742a721041
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-10-16 19:47:20 +0200

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -18,6 +18,7 @@ puts "\e[35m\n
 ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝╚═╝  ╚═╝╚═════╝ 
 \n\e[0m"
 
+
 if ARGV[1] == "firefox"
 
 	i = 0
@@ -101,20 +102,8 @@ end
 
 if ARGV[1] == "assetenum"
 
-	if File.directory?('subdomains') == false
-		system "mkdir subdomains"
-	end
-	
-	if File.directory?('httprobe') == false
-		system "mkdir httprobe"
-	end
-	
-	if File.directory?('nuclei') == false
-		system "mkdir nuclei"
-	end
-	
-	if File.directory?('naabu') == false
-		system "mkdir naabu"
+	if File.directory?('output') == false
+		system "mkdir output"
 	end
 
 	File.open(ARGV[0],'r').each_line do |f|
@@ -123,19 +112,25 @@ if ARGV[1] == "assetenum"
 		
 		#== amass ==
 		puts "\n[\e[34m+\e[0m] Enumerating subdomains for " + target + " with amass"
-		system "amass enum -brute -active -d " + target + " -o subdomains/" + target + ".txt -v"
+		system "amass enum -brute -active -d " + target + " -o output/" + target + ".txt -v"
 
 		#== subfinder ==
 		puts "\n[\e[34m+\e[0m] Enumerating subdomains for " + target + " with subfinder"
-		system "subfinder -d " + target + " -all -o subdomains/" + target + "_subfinder.txt"
-		puts "\n[\e[34m+\e[0m] Adding new subdomains to subdomains/" + target + ".txt with anew"
-		system "type subdomains\\" + target + "_subfinder.txt | anew subdomains/" + target + ".txt"
+		system "subfinder -d " + target + " -all -o output/" + target + "_subfinder.txt"
+		
+		puts "\n[\e[34m+\e[0m] Adding new subdomains to output/" + target + ".txt with anew"
+		system "type output\\" + target + "_subfinder.txt | anew output/" + target + ".txt"
+		
+		File.delete("output/" + target + "_subfinder.txt") if File.exists? "output/" + target + "_subfinder.txt"
 		
 		#== github-subdomains ==
 		puts "\n[\e[34m+\e[0m] Enumerating subdomains for " + target + " with github-subdomains"
-		system "github-subdomains -t github-token.txt -d " + target + " -o subdomains/" + target + "_github.txt"
-		puts "\n[\e[34m+\e[0m] Adding new subdomains to subdomains/" + target + ".txt with anew"
-		system "type subdomains\\" + target + "_github.txt | anew subdomains/" + target + ".txt"
+		system "github-subdomains -t github-token.txt -d " + target + " -o output/" + target + "_github.txt"
+		
+		puts "\n[\e[34m+\e[0m] Adding new subdomains to output/" + target + ".txt with anew"
+		system "type output\\" + target + "_github.txt | anew output/" + target + ".txt"
+		
+		File.delete("output/" + target + "_github.txt") if File.exists? "output/" + target + "_github.txt"
 		
 		#== crt.sh ==
 		puts "\n[\e[34m+\e[0m] Enumerating subdomains for " + target + " with crt.sh"
@@ -153,19 +148,22 @@ if ARGV[1] == "assetenum"
 			end
 
 			crtsh_o.close unless crtsh_o.nil? or crtsh_o.closed?
-
-			puts "\n[\e[34m+\e[0m] Adding new subdomains to subdomains/" + target + ".txt with anew"
-			system "type subdomains\\" + target + "_crtsh.txt | anew subdomains/" + target + ".txt"
+			
+			puts "\n[\e[34m+\e[0m] Adding new subdomains to output/" + target + ".txt with anew"
+			system "type output\\" + target + "_crtsh.txt | anew output/" + target + ".txt"
+			
+			File.delete("output/" + target + "_crtsh.txt") if File.exists? "output/" + target + "_crtsh.txt"
+			
 		rescue
 			puts "[\e[31m" + i.to_s + "\e[0m] ERROR while trying to retrieve information from crt.sh"
 		end
 		
 		#== gobuster + all.txt ==
 		puts "\n[\e[34m+\e[0m] Enumerating subdomains for " + target + " with gobuster and all.txt"
-		system "gobuster dns -d " + target + " -v -t 150 --no-color -o subdomains/" + target + "_gobuster_tmp.txt -w all.txt"
+		system "gobuster dns -d " + target + " -v -t 150 --no-color -o output/" + target + "_gobuster_tmp.txt -w all.txt"
 		
-		gobuster_o = File.new("subdomains/" + target + "_gobuster.txt", 'w')
-		gobuster_tmp = File.open("subdomains/" + target + "_gobuster_tmp.txt",'r')
+		gobuster_o = File.new("output/" + target + "_gobuster.txt", 'w')
+		gobuster_tmp = File.open("output/" + target + "_gobuster_tmp.txt",'r')
 		
 		gobuster_tmp.each_line do |f|
 			if f.include? "Found: "
@@ -174,38 +172,43 @@ if ARGV[1] == "assetenum"
 		end
 		
 		gobuster_tmp.close unless gobuster_tmp.nil? or gobuster_tmp.closed?
-		File.delete("subdomains/" + target + "_gobuster_tmp.txt") if File.exists? "subdomains/" + target + "_gobuster_tmp.txt"
+		File.delete("output/" + target + "_gobuster_tmp.txt") if File.exists? "output/" + target + "_gobuster_tmp.txt"
 		gobuster_o.close unless gobuster_o.nil? or gobuster_o.closed?
 		
-		puts "\n[\e[34m+\e[0m] Adding new subdomains to subdomains/" + target + ".txt with anew"
-		system "type subdomains\\" + target + "_gobuster.txt | anew subdomains/" + target + ".txt"
+		puts "\n[\e[34m+\e[0m] Adding new subdomains to output/" + target + ".txt with anew"
+		system "type output\\" + target + "_gobuster.txt | anew output/" + target + ".txt"
+		
+		File.delete("output/" + target + "_gobuster.txt") if File.exists? "output/" + target + "_gobuster.txt"
 		
 		#== anew final ==
-		puts "\n[\e[34m+\e[0m] Results saved as subdomains/" + target + ".txt"
-		puts "\n[\e[34m+\e[0m] Saving all results for " + ARGV[0] + " in subdomains/allsubs_" + ARGV[0]
-		system "type subdomains\\" + target + ".txt | anew subdomains/allsubs_" + ARGV[0]
+		
+		puts "[\e[34m+\e[0m] Results for " + target + " saved as output/" + target + ".txt"
+		
+		puts "\n[\e[34m+\e[0m] Adding the results for " + target + " in output/allsubs_" + ARGV[0]
+		system "type output\\" + target + ".txt | anew output/allsubs_" + ARGV[0]
+		puts "[\e[34m+\e[0m] Results for " + ARGV[0] + " saved as output/" + ARGV[0] + ".txt"
 
 	end
 	
 	#== httprobe ==
 	puts "[\e[34m+\e[0m] Checking subdomains/allsubs_" + ARGV[0] + " with httprobe"
-	system "type subdomains\\allsubs_" + ARGV[0] + " | httprobe -p http:81 -p http:3000 -p https:3000 -p http:3001 -p https:3001 -p http:8000 -p http:8080 -p https:8443 -c 150 > httprobe/httprobed_" + ARGV[0]
-	puts "[\e[34m+\e[0m] Results saved as httprobe/httprobed_" + ARGV[0]
-	
-	#== nuclei ==
-	puts "[\e[34m+\e[0m] Checking for exposed .git and takeovers with nuclei in" + ARGV[0]
-	system "nuclei -l httprobe/httprobed_" + ARGV[0] + " -t %USERPROFILE%/nuclei-templates/takeovers -t %USERPROFILE%/nuclei-templates/exposures/configs/git-config.yaml -o nuclei/nuclei_" + ARGV[0]
-	puts "[\e[34m+\e[0m] Results saved as nuclei/nuclei_" + ARGV[0]
+	system "type subdomains\\allsubs_" + ARGV[0] + " | httprobe -p http:81 -p http:3000 -p https:3000 -p http:3001 -p https:3001 -p http:8000 -p http:8080 -p https:8443 -c 150 > output/httprobed_" + ARGV[0]
+	puts "[\e[34m+\e[0m] Results saved as output/httprobed_" + ARGV[0]
 	
 	#== naabu ==
-	puts "[\e[34m+\e[0m] Searching for more open ports in subdomains/allsubs_" + ARGV[0] + " with naabu"
-	system "naabu -v -list subdomains/allsubs_" + ARGV[0] + " -exclude-ports 80,443,81,3000,3001,8000,8080,8443 -c 1000 -rate 7000 -stats -o naabu/naabu_" + ARGV[0]
-	puts "[\e[34m+\e[0m] Results saved as naabu/naabu_" + ARGV[0]
+	puts "[\e[34m+\e[0m] Searching for more open ports in output/allsubs_" + ARGV[0] + " with naabu"
+	system "naabu -v -list subdomains/allsubs_" + ARGV[0] + " -exclude-ports 80,443,81,3000,3001,8000,8080,8443 -c 1000 -rate 7000 -stats -o output/naabu_" + ARGV[0]
+	puts "[\e[34m+\e[0m] Results saved as output/naabu_" + ARGV[0]
 	
 	#== naabu | httprobe ==
 	puts "[\e[34m+\e[0m] Checking for hidden web ports in naabu/naabu_" + ARGV[0]
-	system "type naabu\\naabu_" + ARGV[0] + " | httprobe > naabu/naabu_httprobe_" + ARGV[0]
-	puts "[\e[34m+\e[0m] Results saved as naabu/naabu_httprobe_" + ARGV[0]
+	system "type naabu\\naabu_" + ARGV[0] + " | httprobe > output/naabu_httprobe_" + ARGV[0]
+	puts "[\e[34m+\e[0m] Results saved as output/naabu_httprobe_" + ARGV[0]
+	
+	#== nuclei ==
+	puts "[\e[34m+\e[0m] Checking for exposed .git and takeovers with nuclei in" + ARGV[0]
+	system "nuclei -l httprobe/httprobed_" + ARGV[0] + " -t %USERPROFILE%/nuclei-templates/takeovers -t %USERPROFILE%/nuclei-templates/exposures/configs/git-config.yaml -o output/nuclei_" + ARGV[0]
+	puts "[\e[34m+\e[0m] Results saved as output/nuclei_" + ARGV[0]
 	
 end
 
