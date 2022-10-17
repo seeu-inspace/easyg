ba3c5c40975a93aa712fca619349527b123e16f4
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-10-17 15:45:08 +0200

    Update pss.rb

--- scripts/pss.rb
+++ scripts/pss.rb
@@ -2,7 +2,8 @@
 File.open(ARGV[0],'r').each_line do |f|
 	target = f.gsub("\n","").to_s
 	system "python paramspider.py --domain " + target + " --exclude svg,png,gif,ico,jpg,jpeg,bpm,mp3,mp4,ttf,woff,ttf2,woff2,eot,eot2,swf,swf2,pptx,pdf,epub,docx,xlsx,css,txt,js,axd --level high --subs False --output output/" + target + ".txt"
-	if File.exists?("output/" + target.to_s + ".txt") == true
+	if File.exists? "output/" + target.to_s + ".txt"
 		system "type output\\" + target + ".txt | trashcompactor | anew output/" + ARGV[0].gsub('.txt','') + "_final.txt"
+		File.delete("output/" + target.to_s + ".txt")
 	end
 end
