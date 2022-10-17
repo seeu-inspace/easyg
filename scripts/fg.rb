8fe306c2a9a65a726505fa8fe72b57ef83ee1792
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-10-17 16:11:58 +0200

    Update fg.rb

--- scripts/fg.rb
+++ scripts/fg.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 require 'json'
 
-$vulns = JSON.parse('{"SSRF/OR":["dest=","redirect=","uri=","path=","continue=","url=","window=","next=","data=","reference=","site=","html=","val=","validate=","domain=","callback=","return=","page=","feed=","host=","port=","to=","out=","view=","dir=","target=","rurl=","destination=","redir=","redirect_uri=","redirect_url=","image_url=","go=","returnTo=","return_to=","checkout_url=","return_path="],
-"SQLI":["id=","page=","report=","dir=","search=","category=","file=","class","url=","news=","item=","menu=","lang=","name=","ref=","title=","view=","topic=","thread=","type=","date=","form=","main=","nav=","region="],
+$vulns = JSON.parse('{"SQLI":["id=","page=","report=","dir=","search=","category=","file=","class","url=","news=","item=","menu=","lang=","name=","ref=","title=","view=","topic=","thread=","type=","date=","form=","main=","nav=","region="],
 "LFI":["cat=","dir=","action=","board=","date=","detail=","file=","download=","path","folder=","prefix=","include=","page=","inc=","locate=","show=","doc=","site=","type=","view=","content=","document=","layout=","mod=","conf="],
-"RCE":["cmd=","exec=","command=","execute=","ping=","query=","jump=","code=","reg=","do=","func=","arg=","option=","load=","process=","step=","read=","feature=","exe=","module=","payload=","run=","print="]}')
+"RCE":["cmd=","exec=","command=","execute=","ping=","query=","jump=","code=","reg=","do=","func=","arg=","option=","load=","process=","step=","read=","feature=","exe=","module=","payload=","run=","print="],
+"XSS":["q=","s=","search=","id=","lang=","keyword=","query=","page=","keywords=","year=","view=","email=","type=","name=","p=","month=","image=","list_type=","url=","terms=","categoryid=","key=","l=","begindate=","enddate="],
+"SSRF": ["dest=","redirect=","uri=","path=","continue=","url=","window=","next=","data=","reference=","site=","html=","val=","validate=","domain=","callback=","return=","page=","feed=","host=","port=","to=","out=","view=","dir="],
+"Open Redirect": ["next=","url=","target=","rurl=","dest=","destination=","redir=","redirect_uri","redirect_url=","redirect=","out=","view=","to=","image_url=","go=","return=","returnTo=","return_to=","checkout_url=","continue=","return_path="]}')
 
 for i in 0..$vulns.keys.length-1 do
 	File.open(ARGV[0],'r').each_line do |f|
