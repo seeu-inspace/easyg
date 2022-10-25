593631a229f9c531a373c1a9a87364f563eba1c9
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-10-25 19:46:26 +0200

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -150,26 +150,6 @@ if ARGV[1] == "assetenum"
 			puts "[\e[31m" + i.to_s + "\e[0m] ERROR while trying to retrieve information from crt.sh"
 		end
 		
-		#== gobuster + all.txt ==
-		puts "\n[\e[34m+\e[0m] Enumerating subdomains for " + target + " with gobuster and all.txt"
-		system "gobuster dns -d " + target + " -v -t 250 --no-color -o output/" + target + "_gobuster_tmp.txt -w all.txt"
-		
-		gobuster_o = File.new("output/" + target + "_gobuster.txt", 'w')
-		gobuster_tmp = File.open("output/" + target + "_gobuster_tmp.txt",'r')
-		
-		gobuster_tmp.each_line do |f|
-			if f.include? "Found: "
-				gobuster_o.puts f.gsub("Found: ","").downcase
-			end
-		end
-		
-		gobuster_tmp.close unless gobuster_tmp.nil? or gobuster_tmp.closed?
-		File.delete("output/" + target + "_gobuster_tmp.txt") if File.exists? "output/" + target + "_gobuster_tmp.txt"
-		gobuster_o.close unless gobuster_o.nil? or gobuster_o.closed?
-		
-		system "type output\\" + target + "_gobuster.txt | anew output/" + target + "_tmp.txt"
-		File.delete("output/" + target + "_gobuster.txt") if File.exists? "output/" + target + "_gobuster.txt"
-		
 		#== anew final ==
 		
 		puts "\n[\e[34m+\e[0m] Checking if IPs for the subdomains of " + target + " exist"
