35741236ad1c8d2a0b79f6b7ccfdb874a401d708
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-11-02 23:19:58 +0100

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -90,10 +90,10 @@ if ARGV[1] == "crawl"
 		target = f.gsub("\n","").to_s
 		
 		puts "[\e[34m+\e[0m] Crawling " + target + " with hakrawler" + "\n"
-		system 'echo ' + target + '| hakrawler -u -insecure -t 20 -proxy http://localhost:8080'
+		system 'echo ' + target + '| hakrawler -u -insecure -t 20 --cookie "0=1" -proxy http://localhost:8080 '
 		
 		puts "[\e[34m+\e[0m] Crawling " + target + " with gospider" + "\n"
-		system 'gospider -s "' + target + '" -c 10 -d 4 -t 20 --sitemap --other-source -p http://localhost:8080 --blacklist ".(svg|png|gif|ico|jpg|jpeg|bpm|mp3|mp4|ttf|woff|ttf2|woff2|eot|eot2|swf|swf2|css)"'
+		system 'gospider -s "' + target + '" -c 10 -d 4 -t 20 --sitemap --other-source -p http://localhost:8080 -h "Cookie: 0=1" --blacklist ".(svg|png|gif|ico|jpg|jpeg|bpm|mp3|mp4|ttf|woff|ttf2|woff2|eot|eot2|swf|swf2|css)"'
 		
 	end
 end
