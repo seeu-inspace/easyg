32a4e1c2428bf60be4c992cca4e7ced608dadb75
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-11-09 00:37:20 +0100

    Update easyg.rb

--- easyg.rb
+++ easyg.rb
@@ -214,6 +214,9 @@ if ARGV[1] == "assetenum"
 	end
 	
 	#== nuclei ==
+	
+	system "nuclei -update -ut"
+	
 	puts "[\e[34m+\e[0m] Checking for exposed .git and takeovers with nuclei in " + ARGV[0]
 	system "nuclei -l output/httprobe_" + ARGV[0] + " -t %USERPROFILE%/nuclei-templates/takeovers -t %USERPROFILE%/nuclei-templates/exposures/configs/git-config.yaml -t %USERPROFILE%/nuclei-templates/vulnerabilities/generic/crlf-injection.yaml -t %USERPROFILE%/nuclei-templates/exposures/apis/swagger-api.yaml -o output/nuclei_" + ARGV[0]
 	delete_if_empty "output/nuclei_" + ARGV[0]
