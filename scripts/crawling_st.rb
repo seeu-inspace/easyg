3a53040bc313e3a63d3bb0fe752f88d1cc3dad8e
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-12-09 14:25:33 +0100

    Update crawling_st.rb

--- scripts/crawling_st.rb
+++ scripts/crawling_st.rb
@@ -7,7 +7,7 @@ File.open(ARGV[0],'r').each_line do |f|
 	system 'echo ' + target + '| hakrawler -u -insecure -t 20 -proxy http://localhost:8080 -h "Cookie: 0=1"'
 	
 	puts "[\e[34m+\e[0m] Crawling " + target + " with gospider" + "\n"
-	system 'gospider -s "' + target + '" -c 10 -d 4 -t 20 --sitemap --other-source -p http://localhost:8080 --cookie "0=1" --blacklist ".(svg|png|gif|ico|jpg|jpeg|bpm|mp3|mp4|ttf|woff|ttf2|woff2|eot|eot2|swf|swf2|css)"'
+	system 'gospider -s "' + target + '" -c 10 -d 4 -t 20 --sitemap --other-source -p http://localhost:8080 -H "Cookie: 0=1" --blacklist ".(svg|png|gif|ico|jpg|jpeg|bpm|mp3|mp4|ttf|woff|ttf2|woff2|eot|eot2|swf|swf2|css)"'
 	
 	puts "[\e[34m+\e[0m] Crawling " + target + " with katana" + "\n"
 	system 'katana -u "' + target + '" -jc -kf -aff -proxy http://127.0.0.1:8080" -H "Cookie: 0=1"'
