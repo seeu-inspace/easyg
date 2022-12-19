5ccb0ce55e1f0d91beb09adc95fdcc39a8b1e810
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2022-12-19 03:09:37 +0100

    Update nuclei_checks.rb

--- scripts/nuclei_checks.rb
+++ scripts/nuclei_checks.rb
@@ -14,10 +14,10 @@ system "mkdir output" if File.directory?('output') == false
 
 #== nuclei ==	
 puts "[\e[36m+\e[0m] Checking with nuclei in " + ARGV[0]
-system "nuclei -l output/httprobe_" + ARGV[0] + " -t %USERPROFILE%/nuclei-templates/takeovers -t %USERPROFILE%/nuclei-templates/exposures/configs/git-config.yaml -t %USERPROFILE%/nuclei-templates/vulnerabilities/generic/crlf-injection.yaml -t %USERPROFILE%/nuclei-templates/exposures/apis/swagger-api.yaml -t %USERPROFILE%/nuclei-templates/exposed-panels -t %USERPROFILE%/nuclei-templates/miscellaneous/old-copyright.yaml -stats -o output/nuclei_" + ARGV[0]
-delete_if_empty "output/nuclei_" + ARGV[0]
+system "nuclei -l " + ARGV[0] + " -t %USERPROFILE%/nuclei-templates/takeovers -t %USERPROFILE%/nuclei-templates/exposures/configs/git-config.yaml -t %USERPROFILE%/nuclei-templates/vulnerabilities/generic/crlf-injection.yaml -t %USERPROFILE%/nuclei-templates/exposures/apis/swagger-api.yaml -t %USERPROFILE%/nuclei-templates/exposed-panels -t %USERPROFILE%/nuclei-templates/miscellaneous/old-copyright.yaml -stats -o output/nuclei_" + ARGV[0]
+delete_if_empty ARGV[0]
 
 #== check for log4j and cve with Nuclei ==
 puts "[\e[36m+\e[0m] Checking for log4j in " + ARGV[0] + " with nuclei"
-system "nuclei -l output/httprobe_" + ARGV[0] + " -as -tags log4j,cve -stats -o output/nuclei_2_" + ARGV[0]
-delete_if_empty "output/nuclei_2_" + ARGV[0]
+system "nuclei -l " + ARGV[0] + " -as -tags log4j,cve -stats -o output/nuclei_2_" + ARGV[0]
+delete_if_empty ARGV[0]
