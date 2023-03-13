898e8a3c0d5ae8f4979da8029117ce7d778882cb
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2023-03-13 19:51:30 +0100

    Update searchsploit&download.sh

--- scripts/searchsploit&download.sh
+++ scripts/searchsploit&download.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-for e in $(searchsploit $1 -w -t | grep http | cut -f 2 -d "|")
+
+# Define the search term as the first argument
+search_term=$1
+
+# Use searchsploit to search for exploits with the given term, and filter for http entries
+exploit_list=$(searchsploit "$search_term" -w -t | grep http | cut -f 2 -d "|")
+
+# Loop through each exploit in the list
+for exploit in $exploit_list
 do
-	exp_name=$(echo $e | cut -d "/" -f 5)
-	url=$(echo $e | sed 's/exploits/raw/')
-	wget -q --no-check-certificate $url -O $exp_name
+    # Extract the exploit name and raw URL from the current entry
+    exploit_name=$(echo "$exploit" | cut -d "/" -f 5)
+    raw_url=$(echo "$exploit" | sed 's/exploits/raw/')
+
+    # Download the exploit file using wget, with a quiet flag and no certificate check
+    wget -q --no-check-certificate "$raw_url" -O "$exploit_name"
 done
