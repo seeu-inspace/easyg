09734aa21a5f449140d58526a09a50cb6418ac18
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2023-03-26 11:23:46 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,4 +10,5 @@ if "%GOPATH%" == "" (
 	go install -v github.com/tomnomnom/anew@latest
 	go install -v github.com/tomnomnom/httprobe@latest
 	go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+	go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 )
