06e3c4bc51fd0d5aa1d2622e7938c4e3c12a6df4
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2023-12-03 14:14:29 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,14 +3,14 @@
 if [ -z "$GOPATH" ]; then
 	echo "It looks like go is not installed, install it now and then rerun this script."
 else
-	go install -v github.com/OWASP/Amass/v3/...@master
+	go install -v github.com/owasp-amass/amass/v4/...@master
 	go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 	go install -v github.com/gwen001/github-subdomains@latest
 	go install -v github.com/OJ/gobuster/v3@latest
 	go install -v github.com/tomnomnom/anew@latest
 	go install -v github.com/tomnomnom/httprobe@latest
 	go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-	go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+	go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
  	go install github.com/projectdiscovery/katana/cmd/katana@latest
   	go install github.com/jaeles-project/gospider@latest
    	go install github.com/hakluke/hakrawler@latest
