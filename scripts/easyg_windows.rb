8b9fb449d34865b4edb781969e02d9578c2fd9f2
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2023-12-13 12:37:36 +0100

    Update easyg_windows.rb

--- scripts/easyg_windows.rb
+++ scripts/easyg_windows.rb
@@ -372,7 +372,7 @@ if option == "crawl"
 		system 'gospider -s "' + target + '" -c 10 -d 4 -t 20 --sitemap --other-source -p http://localhost:8080 -H "Cookie: 0=1" --blacklist ".(svg|png|gif|ico|jpg|jpeg|bpm|mp3|mp4|ttf|woff|ttf2|woff2|eot|eot2|swf|swf2|css)"'
 		
 		puts "[\e[34m+\e[0m] Crawling " + target + " with katana" + "\n"
-		system 'katana -u "' + target + '" -jc -kf -aff -proxy http://127.0.0.1:8080" -H "Cookie: 0=1"'
+		system 'katana -u "' + target + '" -jc -kf -aff -proxy http://127.0.0.1:8080 -H "Cookie: 0=1"'
 	end
 	
 end
