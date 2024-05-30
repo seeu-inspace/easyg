80f48365cf9c66b7a74f41fe8c954f1b54cd0d62
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2024-05-30 23:14:27 +0200

    Update easyg.rb

--- scripts/easyg.rb
+++ scripts/easyg.rb
@@ -48,8 +48,8 @@ end
 
 
 def adding_anew(file_tmp,file_final)
-	system "cat " + file_tmp + " | anew " + file_final
-	File.delete(file_tmp) if File.exists? file_tmp
+	system "cat #{file_tmp} | anew #{file_final}"
+	File.delete(file_tmp) if File.exists?(file_tmp)
 end
 
 
@@ -59,7 +59,7 @@ def delete_if_empty(file)
 		puts "[\e[36m+\e[0m] No result found"
 		File.delete(file) if File.exists?(file)
 	else
-		puts "[\e[36m+\e[0m] Results added at " + file
+		puts "[\e[36m+\e[0m] Results added at #{file}"
 	end
 end
 
@@ -282,7 +282,7 @@ def search_for_vulns(file_to_scan)
 			modified_url = replace_param_with_fuzz(line)
 			file.puts modified_url
 			rescue Exception => e
-				puts "[\e[31m+\e[0m] ERROR: " + e.message
+				puts "[\e[31m+\e[0m] ERROR: #{e.message}"
 			end
 		end
 	end
@@ -346,8 +346,8 @@ def firefox_fun(params)
 	File.open(params[:file],'r').each_line do |f|
 		target = f.gsub("\n","").to_s
 		i += 1
-		puts "[\e[36m#{i.to_s}\e[0m] Firefox open > " + target
-		system 'firefox "' + target + '"'
+		puts "[\e[36m#{i.to_s}\e[0m] Firefox open > #{target}"
+		system "firefox \"#{target}\""
 		sleep 30 if i%20==0
 	end
 end
@@ -365,17 +365,17 @@ def get_to_burp_fun(params)
 
 			res = request_fun(URI.parse(f.gsub("\n","").to_s))
 
-			puts "[\e[36m#{i.to_s}\e[0m] GET > " + f.gsub("\n","").to_s
+			puts "[\e[36m#{i.to_s}\e[0m] GET > #{f.gsub("\n","").to_s}"
 			i += 1
 
 			while res.is_a?(Net::HTTPRedirection) && redirect > 0
-				puts "	Redirecting to > " + res['location'].to_s
+				puts "	Redirecting to > #{res['location'].to_s}"
 				res = request_fun(URI.parse(res['location']))
 				redirect -= 1
 			end
 
 		rescue Exception => e
-			puts "[\e[31m+\e[0m] ERROR: " + e.message
+			puts "[\e[31m+\e[0m] ERROR: #{e.message}"
 		end
 	end
 
@@ -396,69 +396,69 @@ def assetenum_fun(params)
 		#== amass ==
 
 		if params[:gb_opt] == "y"
-			puts "\n[\e[36m+\e[0m] Enumerating subdomains for " + target + " with amass"
-			system "amass enum -brute -active -d " + target + " -v -dns-qps 200"
-			system "oam_subs -names -d " + target + " > output/" + target + "_tmp.txt"
+			puts "\n[\e[36m+\e[0m] Enumerating subdomains for #{target} with amass"
+			system "amass enum -brute -active -d #{target} -v -dns-qps 200"
+			system "oam_subs -names -d #{target} > output/#{target}_tmp.txt"
 		else
-			puts "\n[\e[36m+\e[0m] Enumerating subdomains for " + target + " with amass"
-			system "amass enum -passive -d " + target + " -v -timeout 15 -dns-qps 200"
-			system "oam_subs -names -d " + target + " > output/" + target + "_tmp.txt"
+			puts "\n[\e[36m+\e[0m] Enumerating subdomains for #{target} with amass"
+			system "amass enum -passive -d #{target} -v -timeout 15 -dns-qps 200"
+			system "oam_subs -names -d #{target} > output/#{target}_tmp.txt"
 		end
 
 		#== subfinder ==
-		puts "\n[\e[36m+\e[0m] Enumerating subdomains for " + target + " with subfinder"
-		system "subfinder -d " + target + " -all -o output/" + target + "_subfinder.txt"
+		puts "\n[\e[36m+\e[0m] Enumerating subdomains for #{target} with subfinder"
+		system "subfinder -d #{target} -all -o output/#{target}_subfinder.txt"
 
-		adding_anew("output/" + target + "_subfinder.txt", "output/" + target + "_tmp.txt")
+		adding_anew("output/#{target}_subfinder.txt", "output/#{target}_tmp.txt")
 
 		#== github-subdomains ==
 		if $config['github_token'] != nil || $config['github_token'] != "YOUR_GITHUB_TOKEN_HERE"
-			puts "\n[\e[36m+\e[0m] Enumerating subdomains for " + target + " with github-subdomains"
-			system "github-subdomains -t #{$config['github_token']} -d " + target + " -o output/" + target + "_github.txt"
-			adding_anew("output/" + target + "_github.txt", "output/" + target + "_tmp.txt")
+			puts "\n[\e[36m+\e[0m] Enumerating subdomains for #{target} with github-subdomains"
+			system "github-subdomains -t #{$config['github_token']} -d #{target} -o output/#{target}_github.txt"
+			adding_anew("output/#{target}_github.txt", "output/#{target}_tmp.txt")
 		end
 
 		#== crt.sh ==
-		puts "\n[\e[36m+\e[0m] Enumerating subdomains for " + target + " with crt.sh"
+		puts "\n[\e[36m+\e[0m] Enumerating subdomains for #{target} with crt.sh"
 
 		begin
-			uri = URI.parse("https://crt.sh/?q=" + target + "&output=json")
+			uri = URI.parse("https://crt.sh/?q=#{target}&output=json")
 			response = Net::HTTP.get_response(uri)
 			crtsh = JSON.parse((response.body).to_s)
 
-			crtsh_o = File.new("output/" + target + "_crtsh.txt", "w")
+			crtsh_o = File.new("output/#{target}_crtsh.txt", "w")
 
 			crtsh.each do | f |
 				puts f["common_name"].gsub('*.','').to_s
-				if f.include? "." + target
+				if f.include? ".#{target}"
 					crtsh_o.puts f["common_name"].gsub('*.','').to_s
 				end
 			end
 
 			crtsh_o.close unless crtsh_o.nil? or crtsh_o.closed?
 
-			adding_anew("output/" + target + "_crtsh.txt", "output/" + target + "_tmp.txt")
+			adding_anew("output/#{target}_crtsh.txt", "output/#{target}_tmp.txt")
 
 		rescue Exception => e
-			puts "[\e[31m+\e[0m] ERROR: " + e.message
+			puts "[\e[31m+\e[0m] ERROR: #{e.message}"
 		end
 
 		#== gobuster ==
 
 		if params[:gb_opt] == "y"
 
-			if !File.exists? "all.txt"
+			if !File.exists?("all.txt")
 				uri = URI.parse("https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt")
 				response = Net::HTTP.get_response(uri)
 				alltxt = (response.body).to_s
 				File.open('all.txt', 'w') { |file| file.write(alltxt) }
 			end
 		
-			puts "\n[\e[36m+\e[0m] Enumerating subdomains for " + target + " with gobuster and all.txt"
-			system "gobuster dns -d " + target + " -v -t 250 --no-color --wildcard -o output/" + target + "_gobuster_tmp.txt -w all.txt"
+			puts "\n[\e[36m+\e[0m] Enumerating subdomains for #{target} with gobuster and all.txt"
+			system "gobuster dns -d #{target} -v -t 250 --no-color --wildcard -o output/#{target}_gobuster_tmp.txt -w all.txt"
 
-			gobuster_o = File.new("output/" + target + "_gobuster.txt", 'w')
-			gobuster_tmp = File.open("output/" + target + "_gobuster_tmp.txt",'r')
+			gobuster_o = File.new("output/#{target}_gobuster.txt", 'w')
+			gobuster_tmp = File.open("output/#{target}_gobuster_tmp.txt",'r')
 
 			gobuster_tmp.each_line do |f|
 				if f.include? "Found: "
@@ -467,19 +467,19 @@ def assetenum_fun(params)
 			end
 
 			gobuster_tmp.close unless gobuster_tmp.nil? or gobuster_tmp.closed?
-			File.delete("output/" + target + "_gobuster_tmp.txt") if File.exists? "output/" + target + "_gobuster_tmp.txt"
+			File.delete("output/#{target}_gobuster_tmp.txt") if File.exists?("output/#{target}_gobuster_tmp.txt")
 
 			gobuster_o.close unless gobuster_o.nil? or gobuster_o.closed?
-			adding_anew("output/" + target + "_gobuster.txt", "output/" + target + "_tmp.txt")
+			adding_anew("output/#{target}_gobuster_tmp.txt", "output/#{target}_gobuster_tmp.txt")
 
 		end
 
 		#== anew final ==
 
-		puts "\n[\e[36m+\e[0m] Checking if IPs for the subdomains of " + target + " exist"
+		puts "\n[\e[36m+\e[0m] Checking if IPs for the subdomains of #{target} exist"
 
-		allsubs_final = File.new("output/" + target + ".txt", 'w')
-		allsubs_tmp = File.open("output/" + target + "_tmp.txt",'r')
+		allsubs_final = File.new("output/#{target}.txt", 'w')
+		allsubs_tmp = File.open("output/#{target}_tmp.txt",'r')
 
 		allsubs_tmp.each_line do |line|
 			begin
@@ -496,55 +496,55 @@ def assetenum_fun(params)
 		end
 
 		allsubs_tmp.close unless allsubs_tmp.nil? or allsubs_tmp.closed?
-		File.delete("output/" + target + "_tmp.txt") if File.exists? "output/" + target + "_tmp.txt"
+		File.delete("output/#{target}_tmp.txt") if File.exists?("output/#{target}_tmp.txt")
 		allsubs_final.close unless allsubs_final.nil? or allsubs_final.closed?
 
-		puts "[\e[36m+\e[0m] Results for " + target + " saved as output/" + target + ".txt"
+		puts "[\e[36m+\e[0m] Results for #{target} saved as output/#{target}.txt"
 
-		puts "\n[\e[36m+\e[0m] Adding the results for " + target + " to output/allsubs_" + file
-		system "cat output/" + target + ".txt | anew output/allsubs_" + file
-		puts "[\e[36m+\e[0m] Results for " + file + " saved as output/allsubs_" + file
+		puts "\n[\e[36m+\e[0m] Adding the results for #{target} to output/allsubs_#{file}"
+		system "cat output/#{target}.txt | anew output/allsubs_#{file}"
+		puts "[\e[36m+\e[0m] Results for #{file} saved as output/allsubs_#{file}"
 
 	end
 
 	#== httpx ==
-	puts "\n[\e[36m+\e[0m] Checking output/allsubs_" + file + " with httpx"
-	system "cat output/allsubs_" + file + " | httpx-toolkit -p 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3001,3002,3003,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9092,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 -o output/httpx_" + file
-	puts "[\e[36m+\e[0m] Results saved as output/httpx_" + file
+	puts "\n[\e[36m+\e[0m] Checking output/allsubs_#{file} with httpx"
+	system "cat output/allsubs_#{file} | httpx-toolkit -p 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3001,3002,3003,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9092,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 -o output/httpx_#{file}"
+	puts "[\e[36m+\e[0m] Results saved as output/httpx_#{file}"
 
 	#== naabu ==
 	if params[:gb_opt] == "y"
-		puts "\n[\e[36m+\e[0m] Searching for more open ports in output/allsubs_" + file + " with naabu"
-		system "naabu -v -list output/allsubs_" + file + " -p - -exclude-ports 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3001,3002,3003,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9092,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 -c 1000 -rate 7000 -stats -o output/naabu_" + file
-		delete_if_empty "output/naabu_" + file
+		puts "\n[\e[36m+\e[0m] Searching for more open ports in output/allsubs_#{file} with naabu"
+		system "naabu -v -list output/allsubs_#{file} -p - -exclude-ports 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3001,3002,3003,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9092,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 -c 1000 -rate 7000 -stats -o output/naabu_#{file}"
+		delete_if_empty "output/naabu_#{file}"
 	end
 
 	#== naabu | httpx ==
-	if File.exists? "output/naabu_" + file
-		puts "\n[\e[36m+\e[0m] Checking for hidden web ports in output/naabu_" + file
-		system "cat output/naabu_" + file + " | httpx-toolkit -o output/httpx_naabu_" + file
+	if File.exists?("output/naabu_#{file}")
+		puts "\n[\e[36m+\e[0m] Checking for hidden web ports in output/naabu_#{file}"
+		system "cat output/naabu_#{file} | httpx-toolkit -o output/httpx_naabu_#{file}"
 
-		if File.exists? "output/httpx_naabu_" + file
-			system "cat output/httpx_naabu_" + file
-			adding_anew("output/httpx_naabu_" + file, "output/httpx_" + file)
-			puts "[\e[36m+\e[0m] Results added at output/httpx_" + file
+		if File.exists?("output/httpx_naabu_#{file}")
+			system "cat output/httpx_naabu_#{file}"
+			adding_anew("output/httpx_naabu_#{file}", "output/httpx_#{file}")
+			puts "[\e[36m+\e[0m] Results added at output/httpx_#{file}"
 		end
 	end
 
 	#== interesting subs ==
 
 	puts "\n[\e[36m+\e[0m] Showing some interesting subdomains found"
-	system "cat output/allsubs_" + file + " | grep -E \"jenkins|jira|gitlab|github|sonar|bitbucket|travis|circleci|eslint|pylint|junit|testng|pytest|jest|selenium|appium|postman|newman|cypress|seleniumgrid|artifactory|nexus|ansible|puppet|chef|deploybot|octopus|prometheus|grafana|elk|slack|admin|teams\" | sort -u > output/interesting_subdomains_" + file
-	system "cat output/interesting_subdomains_" + file
-	delete_if_empty "output/interesting_subdomains_" + file
+	system "cat output/allsubs_#{file} | grep -E \"jenkins|jira|gitlab|github|sonar|bitbucket|travis|circleci|eslint|pylint|junit|testng|pytest|jest|selenium|appium|postman|newman|cypress|seleniumgrid|artifactory|nexus|ansible|puppet|chef|deploybot|octopus|prometheus|grafana|elk|slack|admin|teams\" | sort -u > output/interesting_subdomains_#{file}"
+	system "cat output/interesting_subdomains_#{file}"
+	delete_if_empty "output/interesting_subdomains_#{file}"
 
 	#== nuclei ==
 	if params[:vl_opt] == "y"
-		puts "\n[\e[36m+\e[0m] Checking with nuclei in " + file
-		system "nuclei -l output/httpx_" + file + " -t ~/.local/nuclei-templates/takeovers -t ~/.local/nuclei-templates/exposures/configs/git-config.yaml -t ~/.local/nuclei-templates/vulnerabilities/crlf/crlf-injection.yaml -t ~/.local/nuclei-templates/exposures/apis/swagger-api.yaml -t ~/.local/nuclei-templates/misconfiguration/put-method-enabled.yaml -stats -o output/nuclei_" + file
-		delete_if_empty "output/nuclei_" + file
+		puts "\n[\e[36m+\e[0m] Checking with nuclei in #{file}"
+		system "nuclei -l output/httpx_#{file} -t ~/.local/nuclei-templates/takeovers -t ~/.local/nuclei-templates/exposures/configs/git-config.yaml -t ~/.local/nuclei-templates/vulnerabilities/crlf/crlf-injection.yaml -t ~/.local/nuclei-templates/exposures/apis/swagger-api.yaml -t ~/.local/nuclei-templates/misconfiguration/put-method-enabled.yaml -stats -o output/nuclei_#{file}"
+		delete_if_empty "output/nuclei_#{file}"
 
-		puts "\n[\e[36m+\e[0m] Searching for 401,403 and bypasses " + file
+		puts "\n[\e[36m+\e[0m] Searching for 401,403 and bypasses #{file}"
 		system "cat output/httpx_#{file} | httpx-toolkit -silent -mc 401,403 | tee output/40X_httpx_#{file}"
 		system "byp4xx -xD -xE -xX -m 2 -L output/40X_httpx_#{file} | grep \"200\" | tee output/byp4xx_results_#{file}"
 		delete_if_empty "output/byp4xx_results_#{file}"
@@ -630,16 +630,16 @@ def crawl_burp_fun(params)
 	File.open(params[:file],'r').each_line do |f|
 		target = f.gsub("\n","").to_s
 
-		puts "\n[\e[36m+\e[0m] Crawling " + target + " with hakrawler" + "\n"
+		puts "\n[\e[36m+\e[0m] Crawling #{target} with hakrawler\n"
 		system 'echo ' + target + "| hakrawler -u -insecure -t 20 -proxy http://#{$config['proxy_addr']}:#{$config['proxy_port']} -h \"Cookie: #{$config['cookie']};;Authorization: #{$config['authorization']}\""
 
-		puts "\n[\e[36m+\e[0m] Crawling " + target + " with gospider" + "\n"
+		puts "\n[\e[36m+\e[0m] Crawling #{target} with gospider\n"
 		system 'gospider -s "' + target + "\" -c 10 -d 4 -t 20 --sitemap --other-source -p http://#{$config['proxy_addr']}:#{$config['proxy_port']} -H \"Cookie: #{$config['cookie']}\" -H \"Authorization: #{$config['authorization']}\" --blacklist \".(svg|png|gif|ico|jpg|jpeg|bpm|mp3|mp4|ttf|woff|ttf2|woff2|eot|eot2|swf|swf2|css)\""
 
-		puts "\n[\e[36m+\e[0m] Crawling " + target + " with katana" + "\n"
+		puts "\n[\e[36m+\e[0m] Crawling #{target} with katana\n"
 		system 'katana -u "' + target + "\" -jc -kf -aff -d 3 -fs rdn -proxy http://#{$config['proxy_addr']}:#{$config['proxy_port']} -H \"Cookie: #{$config['cookie']}\""
 
-		puts "\n[\e[36m+\e[0m] Crawling " + target + " with gau" + "\n"
+		puts "\n[\e[36m+\e[0m] Crawling #{target} with gau\n"
 		system 'echo ' + target + "| gau --blacklist svg,png,gif,ico,jpg,jpeg,bpm,mp3,mp4,ttf,woff,ttf2,woff2,eot,eot2,swf,swf2,css --fc 404 --threads 5 --proxy http://#{$config['proxy_addr']}:#{$config['proxy_port']}"
 
 	end
@@ -660,14 +660,14 @@ def crawl_local_fun(params)
 		target = f.gsub("\n","").to_s
 		target_sanitized = target.gsub(/^https?:\/\//, '').gsub(/:\d+$/, '').gsub('/','')
 
-		puts "\n[\e[36m+\e[0m] Crawling #{target} with katana" + "\n"
+		puts "\n[\e[36m+\e[0m] Crawling #{target} with katana\n"
 		system "katana -u #{target} -jc -kf -aff -H \"Cookie: #{$config['cookie']}\" -d 3 -fs fqdn -o output/#{target_sanitized}_tmp.txt"
 		
-		puts "\n[\e[36m+\e[0m] Finding more endpoints for #{target} with waymore" + "\n"
+		puts "\n[\e[36m+\e[0m] Finding more endpoints for #{target} with waymore\n"
 		system "waymore -i #{target} -c /home/kali/.config/waymore/config.yml -f -p 5 -mode U -oU output/#{target_sanitized}_waymore.txt"
 		adding_anew("output/#{target_sanitized}_waymore.txt", "output/#{target_sanitized}_tmp.txt")
 
-		puts "\n[\e[36m+\e[0m] Crawling " + target + " with gau" + "\n"
+		puts "\n[\e[36m+\e[0m] Crawling #{target} with gau\n"
 		system 'echo ' + target + "| gau --blacklist svg,png,gif,ico,jpg,jpeg,bpm,mp3,mp4,ttf,woff,ttf2,woff2,eot,eot2,swf,swf2,css --fc 404 --o output/#{target_sanitized}_gau.txt"
 		adding_anew("output/#{target_sanitized}_gau.txt", "output/#{target_sanitized}_tmp.txt")
 
