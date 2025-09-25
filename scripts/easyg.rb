64bf673691c25d12b6cfbf9afceeef680e379676
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2025-09-25 13:57:19 +0200

    Update easyg.rb

--- scripts/easyg.rb
+++ scripts/easyg.rb
@@ -531,7 +531,7 @@ def clean_urls(file_path, num_threads = $CONFIG['n_threads'])
 	end
 	puts "[\e[32m+\e[0m] URLs with only tracking parameters removed"
 
-	# Step 6: Remove dead links and 404 URLs
+	# Step 6: Remove 4** URLs
 	puts "[\e[34m*\e[0m] Checking for dead links and 404 URLs..."
 	live_urls = process_urls(file_path) do |response, url|
 		if response.is_a?(Net::HTTPSuccess)
@@ -540,9 +540,15 @@ def clean_urls(file_path, num_threads = $CONFIG['n_threads'])
 		elsif response.code.to_i == 404
 			puts "[\e[31m+\e[0m] 404 Not Found: #{url}"
 			nil
-		else
-			puts "[\e[31m+\e[0m] Dead URL: #{url}"
+		elsif response.code.to_i == 410
+			puts "[\e[31m!\e[0m] 410 Gone: #{url}"
 			nil
+		elsif response.code.to_i == 401
+			puts "[\e[31m!\e[0m] 401 Unauthorized: #{url}"
+			nil
+		else
+			puts "[\e[33m~\e[0m] #{response.code} response: #{url}"
+			url
 		end
 	end
 
